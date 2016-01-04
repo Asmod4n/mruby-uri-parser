4bc1ce41467c0f2505d4aff3e191ea66e90c8a00
Asmod4n <Asmod4n@users.noreply.github.com>
2016-01-04 14:10:05 +0100

    add http connect host parsing test

--- test/uri.rb
+++ test/uri.rb
@@ -7,4 +7,10 @@ assert("Uri#parse") do
   assert_equal('/path', uri.path)
   assert_equal('query=string', uri.query)
   assert_equal('fragment', uri.fragment)
+  assert_equal("http://user:password@domain.tld:8080/path?query=string#fragment", uri.to_s)
+
+  #HTTP CONNECT parsing
+  uri = URI.parse("heise.de:443", true)
+  assert_equal('heise.de', uri.host)
+  assert_equal(443, uri.port)
 end
