a035a933430948815613f77b3c97b9c6c83214b8
Asmod4n <Asmod4n@users.noreply.github.com>
2016-04-21 22:04:06 +0200

    closes #1

--- mrblib/uri.rb
+++ mrblib/uri.rb
@@ -1,6 +1,7 @@
 module URI
   class Parsed
     attr_reader :schema, :host, :port, :path, :query, :fragment, :userinfo
+    alias :scheme :schema
 
     def initialize(schema, host, port, path, query, fragment, userinfo, uri)
       @schema, @host, @port, @path, @query, @fragment, @userinfo, @uri = schema, host, port, path, query, fragment, userinfo, uri
