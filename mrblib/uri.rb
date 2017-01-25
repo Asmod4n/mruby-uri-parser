48ec96468d2da9d8edc076a364ef211ce8121a36
Asmod4n <Asmod4n@users.noreply.github.com>
2017-01-25 21:26:48 +0100

    resolve port in c ext, fixes #2

--- mrblib/uri.rb
+++ mrblib/uri.rb
@@ -5,10 +5,6 @@ module URI
 
     def initialize(schema, host, port, path, query, fragment, userinfo, uri)
       @schema, @host, @port, @path, @query, @fragment, @userinfo, @uri = schema, host, port, path, query, fragment, userinfo, uri
-
-      unless port
-        @port = URI.get_port(String(schema).downcase)
-      end
     end
 
     def to_s
