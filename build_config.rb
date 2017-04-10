e1caa57bac635c75e8a1e2bc55b534b2a9b11eec
Asmod4n <Asmod4n@users.noreply.github.com>
2017-04-10 19:16:21 +0200

    remove address sanitizer

--- build_config.rb
+++ build_config.rb
@@ -4,7 +4,5 @@ MRuby::Build.new do |conf|
   conf.cc.defines = %w(ENABLE_DEBUG)
   conf.enable_test
   conf.gembox 'full-core'
-  conf.cc.flags << '-fsanitize=address'
-  conf.linker.flags << '-fsanitize=address'
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
