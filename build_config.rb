6d5babd7aedc769fb73bde80e731747278379957
Asmod4n <Asmod4n@users.noreply.github.com>
2017-04-10 19:13:48 +0200

    add address sanitizer

--- build_config.rb
+++ build_config.rb
@@ -4,5 +4,7 @@ MRuby::Build.new do |conf|
   conf.cc.defines = %w(ENABLE_DEBUG)
   conf.enable_test
   conf.gembox 'full-core'
+  conf.cc.flags << '-fsanitize=address'
+  conf.linker.flags << '-fsanitize=address'
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
