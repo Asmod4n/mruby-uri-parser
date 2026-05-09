f046f296512261c8ab1f7cef0d7cec497fce21f9
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2026-05-09 13:06:57 +0200

    let mruby compile ada

--- build_config.rb
+++ build_config.rb
@@ -1,7 +1,7 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
-  #conf.enable_sanitizer "address,undefined"
+  conf.enable_sanitizer "address,undefined"
   conf.enable_test
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
